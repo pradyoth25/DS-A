65afc470cfb1873c7be0606896b50acdc260fe20
Pradyoth Hegde <pradyoth25@gmail.com>
2021-05-24 10:14:05 -0500

    Update to go from left to right

--- src/main/java/problem_types/LongestCommonSubarray.java
+++ src/main/java/problem_types/LongestCommonSubarray.java
@@ -2,16 +2,14 @@ package problem_types;
 
 public class LongestCommonSubarray {
 
-    public int longestCommonSubArray(String[] s1, String[] s2) {
-        int m = s1.length, n = s2.length;
+    public int longestCommonSubArray(int[] nums1, int[] nums2) {
+        int m = nums1.length, n = nums2.length;
         int[][] dp = new int[m + 1][n + 1];
         int res = 0;
-        for (int i = m - 1; i >= 0; i--) {
-            for (int j = n - 1; j >= 0; j--) {
-                if (s1[i].equals(s2[j])) {
-                    dp[i][j] = dp[i + 1][j + 1] + 1;
-                    res = Math.max(res, dp[i][j]);
-                }
+        for (int i = 1; i <= m; i ++) {
+            for (int j = 1; j <= n; j ++) {
+                if (nums1[i - 1] == nums2[j - 1]) dp[i][j] = 1 + dp[i-1][j-1];
+                res = Math.max(res, dp[i][j]);
             }
         }
         return res;
